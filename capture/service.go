df00f383baea6cf0038ce901d0fb9af9fa099877
Vasyl Osypchuk <leveldorado@gmail.com>
2019-10-19 22:09:02 +0300

    implemented capture.DefaultService make shot and save

--- capture/service.go
+++ capture/service.go
@@ -5,34 +5,52 @@ import (
 	"fmt"
 	"io"
 
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/google/uuid"
+	"github.com/leveldorado/screenshot/store"
 )
 
 type shotMaker interface {
-	MakeShot(ctx context.Context, url string) (io.Reader, error)
+	MakeShot(ctx context.Context, url, format string, quality int) (io.Reader, error)
 }
 
 type fileStore interface {
-	StoreFile(ctx context.Context, file io.Reader, fileID, filename string) (error)
+	StoreFile(ctx context.Context, file io.Reader, fileID, filename string) error
 }
 
-type screenshotMetadata interface {
-
+type screenshotMetadataStore interface {
+	Save(ctx context.Context, doc *store.ScreenshotMetadata) error
 }
 
 type DefaultService struct {
-	sm   shotMaker
-	repo *mongo.Client
+	sm          shotMaker
+	fs          fileStore
+	ms          screenshotMetadataStore
+	shotFormat  string
+	shotQuality int
 }
 
-func NewDefaultService(sm shotMaker) *DefaultService {
-	return &DefaultService{sm: sm}
+func NewDefaultService(sm shotMaker, fs fileStore, ms screenshotMetadataStore, format string, quality int) *DefaultService {
+	return &DefaultService{sm: sm, fs: fs, ms: ms, shotFormat: format, shotQuality: quality}
 }
 
-func (s *DefaultService) MakeShotAndSave(ctx context.Context, url string) error {
-	shot, err := s.sm.MakeShot(ctx, url)
+func (s *DefaultService) MakeShotAndSave(ctx context.Context, url string) (store.ScreenshotMetadata, error) {
+	shot, err := s.sm.MakeShot(ctx, url, s.shotFormat, s.shotQuality)
 	if err != nil {
-		return fmt.Errorf(`failed to make shot: [url: %s, error: %w]`, url, err)
+		return store.ScreenshotMetadata{}, fmt.Errorf(`failed to make shot: [url: %s, error: %w]`, url, err)
 	}
-	s.repo.Database("test").
+	fileID := uuid.New().String()
+	if err = s.fs.StoreFile(ctx, shot, fileID, url); err != nil {
+		return store.ScreenshotMetadata{}, fmt.Errorf(`failed to store file: [id: %s, name: %s, error: %w]`, fileID, url, err)
+	}
+	metadata := store.ScreenshotMetadata{
+		ID:      uuid.New().String(),
+		Url:     url,
+		Format:  s.shotFormat,
+		Quality: s.shotQuality,
+		FileID:  fileID,
+	}
+	if err = s.ms.Save(ctx, &metadata); err != nil {
+		return store.ScreenshotMetadata{}, fmt.Errorf(`failed to save screen shot metadata: [doc: %+v, error: %w]`, metadata, err)
+	}
+	return metadata, nil
 }
--- store/screen_shot.go
+++ store/screen_shot.go
@@ -3,5 +3,8 @@ package store
 type ScreenshotMetadata struct {
 	ID      string `json:"id" bson:"_id"`
 	Url     string `json:"url" bson:"url"`
+	Format  string `json:"format"`
+	Quality int    `json:"quality"`
 	Version int    `json:"version" bson:"version"`
+	FileID  string `json:"file_id" bson:"file_id"`
 }
