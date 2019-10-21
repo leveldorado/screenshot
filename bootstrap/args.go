f42a2247dd8e0ef307838d943d8db74802036c89
Vasyl Osypchuk <leveldorado@gmail.com>
2019-10-21 20:35:13 +0300

    added docker file

--- bootstrap/args.go
+++ bootstrap/args.go
@@ -18,7 +18,7 @@ type flagOptions struct {
 	Database   string `short:"d" long:"database" description:"database connect url" default:"localhost:27017"`
 	Chrome     string `long:"chrome" description:"headless chrome url" default:"localhost:9222"`
 	Mode       string `short:"m" long:"mode" description:"Supported modes: capture (run only application part which capture screenshots), api (run only application part which receive http requests), standalone: (run both services)" default:"standalone"`
-	Address    string `long:"addr" description:"address (host and port) on which api listen http request" default:":9000"`
+	Address    string `long:"address" description:"address (host and port) on which api listen http request" default:":9000"`
 }
 
 func parseFlags(args []string) (flagOptions, error) {
